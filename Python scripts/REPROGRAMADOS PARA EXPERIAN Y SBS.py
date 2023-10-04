08f6b671b34ad0a069ae02b927676be8440ef841
trevorseether <joseph.montoya@unmsm.edu.pe>
2023-10-04 16:34:53 -0500

    actualización

--- Python scripts/REPROGRAMADOS PARA EXPERIAN Y SBS.py
+++ Python scripts/REPROGRAMADOS PARA EXPERIAN Y SBS.py
@@ -156,15 +156,14 @@ df['4/NID/Número de Documento']               = df['4/NID/Número de Documento'
 
 df['5/NCL/Nombre del deudor']                 = tabla1["Apellidos y Nombres / Razón Social 2/"]
 df['06/CCR/Número de código de la operación'] = tabla1['Nro Prestamo \nFincore']
-df['7/SKCR/Saldo capital de la deuda'] = tabla1["Saldo de colocaciones (créditos directos) 24/"]
+df['7/SKCR/Saldo capital de la deuda']        = tabla1["Saldo de colocaciones (créditos directos) 24/"]
 df['8/TCR/Tipo de crédito según reporte crediticio de deudores']        = tabla1["Tipo de Crédito 19/"]
-df['9/MDREPRP/ Modalidad de reprogramación']  = tabla1["TIPO_REPRO"]
+df['9/MDREPRP/ Modalidad de reprogramación']  = tabla1['9/MDREPRP/ Modalidad de reprogramación'] # = tabla1["TIPO_REPRO"]
 ########################
 df['10/SEGRIESGO/ Segmentación de Riesgos']   = 'modificar' #tabla2["RIESGO_FINAL"] #INFO QUE VIENE DE LA TABLA DE EQUIFAX
 ########################
-df['11/SIN/Ingresos devengados']              = tabla1["""Rendimiento
-Devengado 40/"""]
-df['12/FREP/ Fecha en que se realizó la reprogramación']                 = tabla1["FEC_ULT_REPROG"]
+df['11/SIN/Ingresos devengados']              = tabla1['Rendimiento\nDevengado 40/']
+df['12/FREP/ Fecha en que se realizó la reprogramación'] = tabla1["FEC_ULT_REPROG"]
 df['13/CLADEU/ Clasificación del deudor']     = tabla1["Clasificación del Deudor con Alineamiento 15/"]
 df['14/REP_EME']                              = tabla1["Saldo de colocaciones (créditos directos) 24/"]
 
@@ -176,23 +175,26 @@ df['8/TCR/Tipo de crédito según reporte crediticio de deudores'] = df['8/TCR/T
                                                    13:'13'},
                                                   na_action=None) #EN CASO DE NULO NO HACER NADA
 
+'''
 df['9/MDREPRP/ Modalidad de reprogramación'] = df['9/MDREPRP/ Modalidad de reprogramación'].map(
                                                   {"TIPO 1": '1', #REEMPLAZANDO LOS VALORES POR STRINGS CON CEROS
                                                    "TIPO 2": '2',
                                                    "TIPO 3": '1'},
                                                   na_action=None) #EN CASO DE NULO NO HACER NADA
+'''
 
 df = df.merge(tabla2[['NUMERO DOCUMENTO','NIVEL DE RIESGO']],
-                         left_on=["4/NID/Número de Documento"],
-                         right_on=['NUMERO DOCUMENTO']
-                         ,how='left')
+                         left_on  = ["4/NID/Número de Documento"],
+                         right_on = ['NUMERO DOCUMENTO'],
+                         how      = 'left')
 df.drop(['NUMERO DOCUMENTO'], axis=1, inplace=True)
 df['10/SEGRIESGO/ Segmentación de Riesgos'] = df['NIVEL DE RIESGO']
 df.drop(['NIVEL DE RIESGO'], axis=1, inplace=True)
 
-df['10/SEGRIESGO/ Segmentación de Riesgos'] = df['10/SEGRIESGO/ Segmentación de Riesgos'].map({"BAJO": '1',
-                                                                                               "MEDIO": '2',
-                                                                                               "ALTO": '3',
+
+df['10/SEGRIESGO/ Segmentación de Riesgos'] = df['10/SEGRIESGO/ Segmentación de Riesgos'].map({"BAJO"  : '1',
+                                                                                               "MEDIO" : '2',
+                                                                                               "ALTO"  : '3',
                                                                                                'SIN INFORMACION': '2'}, na_action = None)
 
 df['12/FREP/ Fecha en que se realizó la reprogramación'] = pd.to_datetime(df['12/FREP/ Fecha en que se realizó la reprogramación'], format='%d/%m/%Y')
