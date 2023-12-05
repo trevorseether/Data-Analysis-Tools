6200ff7364824f111c99e312ed20a42b81bdda96
trevorseether <joseph.montoya@unmsm.edu.pe>
2023-12-05 18:10:33 -0500

    actualización

--- Python scripts/KASHIO.py
+++ Python scripts/KASHIO.py
@@ -25,12 +25,12 @@ import datetime
 from colorama import Back # , Style, init, Fore
 
 #%% UBICACIÓN DE LOS ARCHIVOS #################################################
-os.chdir('C:\\Users\\sanmiguel38\\Desktop\\KASHIO\\2023 12\\04 dic')
+os.chdir('C:\\Users\\sanmiguel38\\Desktop\\KASHIO\\2023 12\\05 dic')
 ###############################################################################
 
 #%% NOMBRE ARCHIVO PRINCIPAL
 'NOMBRE DEL ARCHIVO DE HOY' ##########################################
-ARCHIVO_HOY = 'DATA_CLIENTES_COOP.SANMIGUEL_20231204.xlsx'
+ARCHIVO_HOY = 'DATA_CLIENTES_COOP.SANMIGUEL_20231205.xlsx'
 ######################################################################
 
 #%%% lectura del archivo
