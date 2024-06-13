f94dd6e5f946f0f5d003d0c666b8ae18c7e67c61
trevorseether <joseph.montoya@unmsm.edu.pe>
2024-06-13 09:37:01 -0500

    actualización

---
+++ Python scripts/ESPECIFICOS/CONTRASEÑA PARA PDF.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 09:29:26 2024
+
+@author: sanmiguel38
+"""
+
+# pip install pypdf2
+
+# =============================================================================
+# ASIGNADOR DE CONTRASEÑA A PDF
+# =============================================================================
+
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+os.chdir('C:\\Users\\sanmiguel38\\Desktop\\contraseña para pdf')
+
+#%%
+def add_password(input_pdf, output_pdf, password):
+    # Abrir el archivo PDF original
+    with open(input_pdf, 'rb') as file:
+        pdf_reader = PdfReader(file)
+        pdf_writer = PdfWriter()
+
+        # Añadir todas las páginas al PdfWriter
+        for page_num in range(len(pdf_reader.pages)):
+            pdf_writer.add_page(pdf_reader.pages[page_num])
+
+        # Añadir la contraseña
+        pdf_writer.encrypt(user_password=password, owner_password=None, use_128bit=True)
+
+        # Escribir el PDF protegido
+        with open(output_pdf, 'wb') as output_file:
+            pdf_writer.write(output_file)
+
+# Uso de la función
+input_pdf = 'CUL-M2JmODcxMWUtNDllYi00M2Y2LWJkNDEtMWJiYjE4OGFhOTFk.pdf'
+output_pdf = 'pdf_protegido.pdf'
+password = '123'
+
+add_password(input_pdf, output_pdf, password)
+
+
