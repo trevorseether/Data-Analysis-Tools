ce1927b9e05769a97dd2176b009d4483ef8129d2
trevorseether <joseph.montoya@unmsm.edu.pe>
2024-06-24 11:00:05 -0500

    actualización

--- Python scripts/ALINEAMIENTO EXTERNO - COMPRA DE DEUDA.py
+++ Python scripts/ALINEAMIENTO EXTERNO - COMPRA DE DEUDA.py
@@ -18,13 +18,13 @@ warnings.filterwarnings('ignore')
 #%%
 # COLUMNA_ALINEAMIENTO = 'ALINEAMIENTO EXTERNO SBS RCC NOVIEMBRE 2023' # Columna 32 en el excel (no incluye NO REGULADAS)
 
-CORTE_SQL         = '20240430'
+CORTE_SQL         = '20240531'
 
-tabla             = '[ANEXOS_RIESGOS3].[ALINEAMIENTO EXTERNO].[2024_04]' # urgente siempre cambiar esta vaina
+tabla             = '[ANEXOS_RIESGOS3].[ALINEAMIENTO EXTERNO].[2024_05]' # urgente siempre cambiar esta vaina
 
-os.chdir('C:\\Users\\sanmiguel38\\Desktop\\ALINEAMIENTO EXTERNO\\2024\\2024 abril')
+os.chdir('C:\\Users\\sanmiguel38\\Desktop\\ALINEAMIENTO EXTERNO\\2024\\2024 mayo')
 
-NOMBRE_AL_EXTERNO = 'exceldoc_AlinCartera_2171967_42734875_28520249470_1.csv'
+NOMBRE_AL_EXTERNO = 'exceldoc_AlinCartera_2171967_42734875_246202484246_1.csv'
 
 CARGA_SQL_SERVER  = True
 
