d426ae0b239367f97633c1d0d9a048caf5d1d4e2
trevorseether <joseph.montoya@unmsm.edu.pe>
2024-12-06 15:33:56 -0500

    actualización

--- Python scripts/CRUCE DE BAJAS KONECTA mejorado 2.py
+++ Python scripts/CRUCE DE BAJAS KONECTA mejorado 2.py
@@ -24,15 +24,15 @@ from colorama import Back # , Style, init, Fore
 #%% DIRECTORIO DE TRABAJO, fecha actual
 
 'AQUI SE PONE LA FECHA DE HOY' ################################################
-FECHATXT = '2024-12-02'  # FORMATO AÑO-MES-DÍA
+FECHATXT = '2024-12-06'  # FORMATO AÑO-MES-DÍA
 ###############################################################################
 
 'directorio de trabajo' #######################################################
-directorio = 'C:\\Users\\sanmiguel38\\Desktop\\BAJAS KONECTA\\2024\\diciembre\\02 12'
+directorio = 'C:\\Users\\sanmiguel38\\Desktop\\BAJAS KONECTA\\2024\\diciembre\\06 12'
 ###############################################################################
 
 'NOMBRE DEL ARCHIVO DE BAJAS ENVIADO' #########################################
-nombre_archivo = '1ER INFORME 12_24 GRUPO KONECT.xlsx'
+nombre_archivo = '2DO INFORME 12_24 GRUPO KONECT.xlsx'
 ###############################################################################
 
 'filas a skipear' ######################
