7c274bf366b1cef31c2f7570c9cb49c495783748
trevorseether <joseph.montoya@unmsm.edu.pe>
2024-12-11 18:03:01 -0500

    actualización

--- Python scripts/KASHIO.py
+++ Python scripts/KASHIO.py
@@ -22,12 +22,12 @@ import datetime
 from colorama import Back # , Style, init, Fore
 
 #%% UBICACIÓN DE LOS ARCHIVOS #################################################
-os.chdir('C:\\Users\\sanmiguel38\\Desktop\\KASHIO\\2024 12\\10 12')
+os.chdir('C:\\Users\\sanmiguel38\\Desktop\\KASHIO\\2024 12\\11 12')
 ###############################################################################
 
 #%% NOMBRE ARCHIVO PRINCIPAL
 'NOMBRE DEL ARCHIVO DE HOY' ###################################################
-ARCHIVO_HOY = 'DATA_CLIENTES_COOP.SANMIGUEL_20241210.xlsx'
+ARCHIVO_HOY = 'DATA_CLIENTES_COOP.SANMIGUEL_20241211.xlsx'
 ###############################################################################
 
 #%% CREAR ARCHIVO DE VERIFICACIÓN DE CORREOS ##################################
